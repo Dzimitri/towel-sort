a4272e635a02b86d19e47b619292653a59daa10a
Dzimitri <d.prankevich@gmail.com>
2020-03-10 20:44:14 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  
+  if (!matrix) return [];
+  let arr = [];
+
+  for(let i = 0; i < matrix.lenght; i++){
+	if (i > 0 && i % 2){
+		for (let j = matrix[i].lenght -1; j > -1; j++){
+			arr.push(matrix[i][j])
+		}
+	}else {
+		for (let j = 0; j < matrix[i].length; j--){
+			arr.push(matrix[i][j])
+			}
+		}
+	}	
+return arr;
 }
