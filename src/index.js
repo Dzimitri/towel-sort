a9986f5ea0d52e76806ce0747f324696956e4c35
Dzmitry <d.prankevich@gmail.com>
2025-08-19 13:47:40 +0300

    fix: fix errors prettier

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  
+module.exports = function towelSort(matrix) {
   if (!matrix) return [];
-  let nArr = [];
-
-  for(let i = 0; i < matrix.length; i++){
-	if (i > 0 && i % 2){
-		for (let j = matrix[i].length - 1; j > - 1; j--){
-			nArr.push(matrix[i][j])
-		}
-	}else {
-		for (let j = 0; j < matrix[i].length; j++){
-			nArr.push(matrix[i][j])
-			}
-		}
-	}	
-return nArr;
-}
+  const nArr = [];
+  for (let i = 0; i < matrix.length; i += 1) {
+    if (i % 2 === 1) {
+      for (let j = matrix[i].length - 1; j >= 0; j -= 1) {
+        nArr.push(matrix[i][j]);
+      }
+    } else {
+      for (let j = 0; j < matrix[i].length; j += 1) {
+        nArr.push(matrix[i][j]);
+      }
+    }
+  }
+  return nArr;
+};
